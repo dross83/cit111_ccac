c3db754d5c603db49e2896763217bcc46ae42143
dross83 <36931657+dross83@users.noreply.github.com>
2018-05-15 01:47:08 -0400

    Add files via upload

---
+++ FinalProject/src/statTracker/Player.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statTracker;
+
+/**
+ *
+ * @author dylan
+ */
+public class Player {
+    // member variables
+    public String name;
+    public int gamesPlayed;
+    public int goals;
+    public int assists;
+    public int pointsPerGameAvg;
+    //method calls
+//    public String getName() {
+//        return name;
+//    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//    public int getGamesPlayed() {
+//        return gamesPlayed;
+//    }
+//    public void setGamesPlayed(int gamesPlayed){
+//        this.gamesPlayed = gamesPlayed;
+//    }
+//    public int getGoals() {
+//        return goals;
+//    }
+//    public void setGoals(int goals){
+//        this.goals = goals;
+//    }
+//    public int getAssists() {
+//        return assists;
+//    }
+//    public void setAssists(int assists){
+//        this.assists = assists;
+//    }
+//    public int getPointsPerGameAvg() {
+//        return pointsPerGameAvg;
+//    }
+//    public void setPointsPerGameAvg(int pointPerGameAvg){
+//        this.pointsPerGameAvg = pointsPerGameAvg;
+//    }
+    
+}//close main
